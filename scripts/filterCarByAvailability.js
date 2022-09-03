8599656f3b9de5db2acd8f6dd30d7709394b0771
hy-reza <hy.rezaalfanda@gmail.com>
2022-09-03 23:16:30 +0700

    filter car function done

---
+++ scripts/filterCarByAvailability.js
@@ -0,0 +1,19 @@
+function filterCarByAvailability(cars) {
+  //error handler using try catch
+  try {
+    //declare result variable
+    const result = [];
+
+    //loop each car then filter it by availability
+    cars.forEach((car) => car.available && result.push(car));
+
+    //return the result
+    return result;
+
+    // on line but cheat :v
+    // return [...cars.filter(car => car.available)]
+  } catch (err) {
+    //show error message
+    console.error(err);
+  }
+}
